893d916cd931d109ada97cc7da84a97f68e6228d
Steven Sosebee <swsosebee@gmail.com>
2023-09-14 15:13:27 -0400

    initial commit

---
+++ pull.php
@@ -0,0 +1,8 @@
+<?php
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+// input the application's home folder
+$folder = "/"
+if ( $_POST['payload'] ) {
+   shell_exec("cd ~/$location && git pull");
+}
+?>
